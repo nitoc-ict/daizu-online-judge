1f9aaf187278b3e048835eb2546bd6df87a51b3b
kurokoji <shu.kakihana@gmail.com>
2018-11-08 23:52:28 +0900

    fix: fix dub test

--- source/app.d
+++ source/app.d
@@ -1,13 +1,12 @@
 import vibe.vibe;
 
-void main() {
+shared static this() {
   auto settings = new HTTPServerSettings;
   settings.port = 8080;
   settings.bindAddresses = ["::1", "127.0.0.1"];
   listenHTTP(settings, &hello);
 
   logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-  runApplication();
 }
 
 void hello(HTTPServerRequest req, HTTPServerResponse res) {
